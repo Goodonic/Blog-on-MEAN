4fb58d52eff9a85348f0b3cac00169a00cd4c646
Никита <orehovnikita94@gmail.com>
2023-01-06 13:12:58 +0500

    Connecting the database

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const express = require("express");
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const body_parser = require("body-parser");
 const cors = require("cors");
 const passport = require("passport");
 const path = require("path");
+// Name and secret key of your database
+const config = require("./config/db")
+
+mongoose.set('strictQuery', false);
 
 const app = express();
 
 const port = 3000;
 
+mongoose.connect(config.db);
+mongoose.connection.on('connected', () => {
+    console.log("Successful connection to the database")
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log("Not successful connection to the database" + err)
+});
 app.listen(port, ()=>{
     console.log("Server working! Port:" + port)
 });
 
-app.get('/', (req, res)=>{
-    res.send("Главная страница");
+app.get('/', (req, res) => {
+    res.send("Home page")
 });
